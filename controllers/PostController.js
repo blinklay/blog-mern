e5f4a69d50e9b238629e5868299818cdc8cd0752
blinklay <danillitvinenkod00@gmail.com>
2025-01-09 20:35:25 +0300

    CRUD to posts

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -2,6 +2,18 @@
 import { validationResult } from "express-validator";
 import Post from "../models/Post.js";
 
+export const getAll = async (req, res) => {
+  try {
+    const posts = await Post.find().populate({ path: "user", select: ["fullname", "avatarUrl"] }).exec()
+    res.status(200).json(posts)
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      message: "Не удалось получить статьи!"
+    })
+  }
+}
+
 export const create = async (req, res) => {
   try {
     const doc = new Post({
@@ -10,11 +22,74 @@ export const create = async (req, res) => {
       imageUrl: req.body.imageUrl,
       tags: req.body.tags,
       user: req.userId,
-    })
+    });
+
+    const post = await doc.save()
+    res.status(200).json(post)
   } catch (error) {
     console.log(error);
     return res.status(500).json({
       message: "Не удалось создать пост!"
+    })
+  }
+}
+
+export const getOne = async (req, res) => {
+  try {
+    const postId = req.params.id
+
+    Post.findOneAndUpdate(
+      { _id: postId }, { $inc: { viewsCount: 1 } }, { returnDocument: "After" })
+      .then(doc => res.json(doc))
+      .catch(err => res.status(500).json({ message: "Статья не найдена" }))
+
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      message: "Не удалось получить статью!"
+    })
+  }
+}
+
+export const remove = async (req, res) => {
+  try {
+    const postId = req.params.id
+    Post.findOneAndDelete({
+      _id: postId
+    })
+      .then(() => res.json({ message: "Статья успешно удалена!" }))
+      .catch(err => res.status(500).json({ message: "Не удалось удалить статью!" }))
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      message: 'Не удалось удалить статью!'
+    })
+  }
+}
+
+export const update = async (req, res) => {
+  try {
+    const postId = req.params.id;
+    Post.findOneAndUpdate({
+      _id: postId
+    }, {
+      title: req.body.title,
+      text: req.body.text,
+      tags: req.body.tags,
+      imageUrl: req.body.imageUrl,
+      user: req.userId
+    })
+      .then(() => res.status(200).json({ message: "Статья успешно обновлена!" }))
+      .catch(err => {
+        console.log(err);
+        return res.status(500).json({
+          message: "Не удалось обновить статью!"
+        })
+      })
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      message: "Не удалось обновить статью!"
     })
   }
 }
--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 import express from "express"
 import mongoose from "mongoose"
-import { loginValidator, registerValidator } from "./validations/auth.js";
+import { loginValidator, postCreateValidator, registerValidator } from "./validations/auth.js";
 import checkAuth from "./utils/checkAuth.js";
 import { getMe, login, register } from "./controllers/UserController.js";
+import { create, getAll, getOne, remove, update } from "./controllers/PostController.js";
 
 mongoose.connect("mongodb+srv://admon:123qwe@cluster0.ajko2.mongodb.net/blog?retryWrites=true&w=majority&appName=Cluster0")
   .then(() => {
@@ -18,6 +19,12 @@ app.use(express.json())
 app.post('/auth/register', registerValidator, register)
 app.post("/auth/login", loginValidator, login)
 app.get("/auth/me", checkAuth, getMe)
+
+app.get("/posts", getAll)
+app.get("/posts/:id", getOne)
+app.post("/posts", checkAuth, postCreateValidator, create)
+app.delete("/posts/:id", checkAuth, remove)
+app.patch("/posts/:id", checkAuth, update)
 
 app.listen(3000, (err) => {
   if (err) {
